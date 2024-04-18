c6aa9e36f568d726802e7a32f3e4ecc518c6987d
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-04-18 18:24:30 +0900

    Lecture3 추가

---
+++ springIdol2/src/main/java/springidol/annotation/SpringIdol.java
@@ -0,0 +1,31 @@
+package springidol.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringIdol implements TalentCompetition {
+	@Autowired		
+	private Performer[] performers;
+
+	public SpringIdol() {
+	}
+	
+	public Performer[] getPerformers() {
+		return performers;
+	}
+
+	public void setPerformers(Performer[] performers) {
+		this.performers = performers;
+	}
+	
+	@Override
+	public void run() {
+		for (int i = 0; i < performers.length; i++) {
+			System.out.println("Performance #" + (i+1));
+			Performer performer = performers[i];
+			performer.perform();
+			System.out.println("-----------------------");
+		}
+	}
+}
