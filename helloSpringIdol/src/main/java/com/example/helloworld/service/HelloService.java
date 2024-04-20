bdf02b2540e6e947d4ff5ab565c8eb985f4f68df
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-04-20 18:33:45 +0900

    2.4
    String makePerformance(String id) 메소드 정의
     주어진 id에 해당하는 performer bean을 Map에서 구함
     그 performer의 공연을 실행하고 결과(문자열)을 반환

--- helloSpringIdol/src/main/java/com/example/helloworld/service/HelloService.java
+++ helloSpringIdol/src/main/java/com/example/helloworld/service/HelloService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class HelloService {
@@ -16,6 +17,20 @@ public class HelloService {
     public HelloService(HashMap<String, Performer> performerHashMap){
         this.performerHashMap=performerHashMap;
     } //생성자에 Autowired 붙여서 id와 Performer Map 자동 주입할거다
+
+    public String makePerformance(String id){
+        Performer performer=null;
+        for (Map.Entry<String,Performer> entry:performerHashMap.entrySet()){
+            String performerId = entry.getKey();
+            Performer p = entry.getValue();
+            if(performerId==id){
+                performer=p; //주어진 id에 해당하는 performer bean을 Map에서 구함
+                break;
+            }
+        }
+        String perf=performer.perform();
+        return perf; //그 performer의 공연을 실행하고 결과(문자열)을 반환
+    }
 
     public String getGreeting() {		// business method
         int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
