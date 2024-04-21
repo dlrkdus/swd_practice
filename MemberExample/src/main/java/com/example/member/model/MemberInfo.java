e34148532888d6e668aef6350631bb0c9cc49a48
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-04-21 17:33:58 +0900

    1
    MemberInfo DTO 수정

--- MemberExample/src/main/java/com/example/member/model/MemberInfo.java
+++ MemberExample/src/main/java/com/example/member/model/MemberInfo.java
@@ -1,67 +1,121 @@
 package com.example.member.model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
 public class MemberInfo {
+
 
 	private String id;
 	private String name;
-	private Address address;	
-	private String email;
 	private String password;
-	private boolean allowNoti;
+	private String phone;
+	private Address address;
+	private String performerType;
+	private String performTitle;
+	private int performTime;
+	private String requestedTerm;
+	private boolean onlinePerform;
 
-	public MemberInfo() {
-	}
 
-	public MemberInfo(String id, String name, String email, String password, boolean alloNoti, Address address) {
+
+	public MemberInfo(String id, String name, String password, String phone, Address address, String performerType,
+					  String performTitle,int performTime,String requestedTerm, boolean onlinePerform) {
+		super();
 		this.id = id;
 		this.name = name;
-		this.email = email;
 		this.password = password;
-		this.allowNoti = alloNoti;
+		this.phone = phone;
 		this.address = address;
+		this.performerType = performerType;
+		this.performTitle = performTitle;
+		this.performTime=performTime;
+		this.requestedTerm=requestedTerm;
+		this.onlinePerform = onlinePerform;
 	}
 
 	public String getId() {
 		return id;
 	}
 
+	public void setId(String id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
 
-	public String getEmail() {
-		return email;
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public boolean isAllowNoti() {
-		return allowNoti;
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 	public Address getAddress() {
 		return address;
 	}
 
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String getPerformTitle() {
+		return performTitle;
+	}
+
+	public void setPerformTitle(String performTitle) {
+		this.performTitle = performTitle;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPerformerType() {
+		return performerType;
+	}
+
+	public void setPerformerType(String performerType) {
+		this.performerType = performerType;
+	}
+
+	public int getPerformTime(){
+		return performTime;
+	}
+
+	public void setPerformTime(int performTime){
+		this.performTime=performTime;
+	}
+	public String getRequestedTerm(){
+		return requestedTerm;
+	}
+	public void setRequestedTerm(String requestedTerm){
+		this.requestedTerm=requestedTerm;
+	}
+
+	public boolean isOnlinePerform() {
+		return onlinePerform;
+	}
+
+	public void setOnlinePerform(boolean onlinePerform) {
+		this.onlinePerform = onlinePerform;
+	}
+
 	public boolean matchPassword(String inputPassword) {
 		return password.equals(inputPassword);
 	}
 
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public void setAllowNoti(boolean allowNoti) {
-		this.allowNoti = allowNoti;
-	}
-
-	public void setAddress(Address address) {
-		this.address = address;
-	}
 }
---
+++ MemberExample/src/main/java/com/example/member/model/PerformerType.java
@@ -0,0 +1,5 @@
+package com.example.member.model;
+
+public enum PerformerType {
+    Instrumentalist, Singer, PoeticJuggler, OneManBand
+}
